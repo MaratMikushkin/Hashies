aef79d2d21a808403d0335ec4cef2289e2ad4485
Alex Rehsitd <qblake.public@gmail.com>
2013-08-02 05:41:23 +0000

    dash initialize method refactoring

--- lib/Hashies/dash.rb
+++ lib/Hashies/dash.rb
@@ -24,34 +24,35 @@ module Hashies
     
       self.class.properties.each do |current_property, options|
         
+        if options.has_key?(:default)
+          @hash[current_property] = options[:default]
+        end
+        def_singleton_methods(current_property)
+        args.each do |arg|
+            @hash[current_property] = arg[current_property] unless arg[current_property].nil?
+        end
         if options.has_key?(:required)
           raise ArgumentError if args.empty?
           args.each do |arg|
-            if arg.keys.include?(current_property) == false
-              raise ArgumentError
-            else
-              @hash[current_property] = arg[current_property]
-              define_singleton_method(current_property) do
-                @hash[current_property]
-              end
+            if arg.has_key?(current_property)
               define_singleton_method("#{current_property.to_s}=".to_sym) do |val|
                 raise ArgumentError if val == nil
                 @hash[current_property] = val
               end
+            else
+              raise ArgumentError
             end
           end
-        else
-          if options.has_key?(:default)
-            @hash[current_property] = options[:default]
-          end
-          
-          define_singleton_method("#{current_property.to_s}=".to_sym) do |val|
-            @hash[current_property] = val
-          end
-          define_singleton_method(current_property) do
-            @hash[current_property]
-          end
         end
+      end
+    end
+
+    def def_singleton_methods(curr_property)
+      define_singleton_method(curr_property) do
+        @hash[curr_property]
+      end
+      define_singleton_method("#{curr_property.to_s}=".to_sym) do |val|
+        @hash[curr_property] = val
       end
     end
   end
