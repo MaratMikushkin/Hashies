736f76ee4a5243b4057396acfd3178fa2f93ea2a
Alex Rehsitd <qblake.public@gmail.com>
2013-08-02 10:34:42 +0000

    Uppercase folders removed

--- lib/Hashies.rb
+++
@@ -1,7 +0,0 @@
-require "Hashies/version"
-
-module Hashies
-  # Your code goes here...
-  autoload :Mash, 'Hashies/mash'
-  autoload :Dash, 'Hashies/dash'
-end
--- lib/Hashies/version.rb
+++
@@ -1,3 +0,0 @@
-module Hashies
-  VERSION = "0.0.1"
-end
